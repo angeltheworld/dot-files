74d4eaab6354ffd46ae526b4576f0b0ad8232273
AngelAlvCam <fluttershady@gmail.com>
2025-09-12 21:52:04 -0600

    modify wallpaper installation

--- setup.sh
+++ setup.sh
@@ -10,18 +10,17 @@ echo 'Packages ready :)'
 # Copy config files
 echo 'Copying configuration files to home directory...'
 mkdir $HOME/.config -p
+
 # Get subdirectories and copy
 subdir=$(ls -l | grep '^d' | cut -d ' ' -f 9)
 cp -r $subdir $HOME/.config
 echo 'Configuration files ready :)'
 
-# Wallpaper
-echo 'Installing wallpaper...'
-mkdir -p $HOME/.config/wallpapers
-curl -O https://i.imgur.com/OIU8OL9.jpeg
-if [ -f "OIU8OL9.jpeg" ]; then
-    mv OIU8OL9.jpeg $HOME/.config/wallpapers/choerry.jpeg
-    echo "Wallpaper ready :)"
-else
-    echo "The specified wallpaper couldn't be downloaded... :("
-fi
+# Wallpapers
+echo 'Installing wallpapers...'
+while IFS= read -r line; do
+        url=$(echo $line | cut -d ' ' -f 1)
+        name=$(echo $line | cut -d ' ' -f 2)
+        wget $url -O $name
+done < wallpapers.txt
+echo 'Wallpapers ready :)'
